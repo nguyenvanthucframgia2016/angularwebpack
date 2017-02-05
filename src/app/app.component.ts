1f8118ec10c23505fd346208be67084420bd6870
thuc <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-05 16:57:20 +0700

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public fontSize = 8;
+    public getStyle() {
+        return {
+            'font-size.px': this.fontSize < 10 ? this.fontSize * 3 : this.fontSize,
+            'color': 'blue',
+            'background': 'url(\'../src/assets/images/image.jpg\') no-repeat'
+        }
+    }
+}
