1b6d3f129a0a68af1a2828b35487169e967cd799
thuc <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-05 14:24:03 +0700

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { DirectionEnum } from './enum/directionEnum';
 
 @Component({
   selector: 'my-app',
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public person = 'A';
+    public users = [
+        { id: 1, name: 'Nguyen Van A' },
+        { id: 2, name: 'Nguyen Van B' },
+        { id: 3, name: 'Nguyen Van C' },
+        { id: 4, name: 'Nguyen Van D' },
+        { id: 'ab', name: 'Nguyen Van E' },
+    ];
+    public dirEnum = DirectionEnum;
+    public north = DirectionEnum.North;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {AlertModule} from 'ng2-bootstrap';
+import { AlertModule } from 'ng2-bootstrap';
 
 @NgModule({
   imports: [
