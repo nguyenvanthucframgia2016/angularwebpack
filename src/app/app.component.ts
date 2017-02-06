f8cb408c74c345d6b45f3e171518ff9b84def5f6
thuc <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-07 00:05:17 +0700

    DecimalPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public e: number = 2.718281828459045;
+    pi: number = 3.141592;
+    num2 = 0.5;
+    num1 = 12.654;
+}
