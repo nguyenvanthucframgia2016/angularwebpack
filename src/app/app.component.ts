11b0332e5f3d97a3b71ff797afee4044c03a83c8
nguyenvanthucframgia2016 <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-08 17:07:06 +0700

    Validate template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    contact: ContactForm = {
+        firstname: 'Thuc',
+        lastname: 'Nguyen Van',
+        address: {
+            city: 'Ha noi',
+            street: 'Ho Tung Mau',
+            postcode: '100000'
+        }
+    };
+
+    onSubmit(contactForm: any) {
+        console.log(contactForm.value);
+    }
+}
+
+class ContactForm {
+    firstname: string;
+    lastname: string;
+    address: {
+        city: string,
+        street: string,
+        postcode: string
+    }
+}
