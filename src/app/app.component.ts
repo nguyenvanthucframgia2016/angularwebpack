49442ac41124199695662c31dbea74843c0620fa
nguyenvanthucframgia2016 <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-08 10:39:54 +0700

    Template driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    contact: Contact = {
+        firstname: 'Thuc',
+        lastname: 'po rao',
+        address: {
+            city: 'Ha noi',
+            street: 'Ho Tung Mau',
+            pincode: '100000'
+        }
+    };
+
+    onSubmit(contactForm: any) {
+        console.log(contactForm.value);
+        // Khi dung two-way binding thi gia tri cua contactForm va this.contact la nhu nhau
+        console.log(this.contact);
+    }
+}
+
+class Contact {
+    firstname: string;
+    lastname: string;
+    address: {
+        city: string,
+        street: string,
+        pincode: string
+    }
+}
