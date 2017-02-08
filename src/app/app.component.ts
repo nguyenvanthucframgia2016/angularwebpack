988f0ecb627efd3e125240094f66470e12ecf2d3
nguyenvanthucframgia2016 <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-08 16:20:26 +0700

    Model Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    contactForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.contactForm = fb.group({
+            firstname: ['', [Validators.required, Validators.minLength(5)]],
+            lastname: [],
+            address: fb.group({
+                city: [],
+                street: [],
+                postcode: []
+            })
+        });
+    }
+
+    onSubmit() {
+        console.log(this.contactForm.value);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AlertModule} from 'ng2-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
-    BrowserModule, AlertModule.forRoot()
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule,
   ],
   declarations: [
     AppComponent
