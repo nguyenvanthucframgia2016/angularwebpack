7205213d0f5c3a7dce99c4c56ef1cb4f5e2f6e2b
nguyenvanthucframgia2016 <nguyen.van.thuc.framgia2016@gmail.com>
2017-02-09 10:14:45 +0700

    Validate model driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './layout/index.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    contactForm: FormGroup;
+    submitted: boolean = false;
+    contact: ContactForm = {
+        firstname: 'Thuc',
+        lastname: 'Nguyen Van',
+        address: {
+            city: 'Ha Noi',
+            street: 'Ho Tung Mau',
+            postcode: '100000'
+        }
+    };
+
+    constructor(private fb: FormBuilder) {
+        this.contactForm = fb.group({
+            firstname: ['', [Validators.required, Validators.minLength(5)]],
+            lastname: [],
+            address: fb.group(
+{                city: [],
+                street: [],
+                postcode: []
+            })
+        });
+    }
+
+    onSubmit() {
+        if (!this.contactForm.valid) {
+            this.submitted = true;
+            return;
+        } else {
+            console.log(this.contactForm.value);
+            console.log(this.contact);
+        }
+    }
+}
+
+class ContactForm {
+    firstname: string;
+    lastname: string;
+    address: {
+        city: string,
+        street: string,
+        postcode: string
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {AlertModule} from 'ng2-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    BrowserModule, AlertModule.forRoot()
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     AppComponent
